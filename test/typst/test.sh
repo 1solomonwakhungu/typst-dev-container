3fc6a23db4aac952e17e02037c07e42427415599
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2025-05-18 23:34:51 -0500

    update 1 file, delete 7 files, create 2 files and move 2 files

--- test/typst/test.sh
+++ test/typst/test.sh
@@ -4,7 +4,10 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
+
+# Typst specific tests
+check "typst help" typst help
+check "typst help watch" typst help watch
 
 # Report result
 reportResults
